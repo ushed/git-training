afffc9eb1f2c5239ac60e92200cf6869b60e1bd8
Shuhei Kawano <shu_0@Shuheis-MacBook-Air.local>
2023-08-10 16:07:01 +0900

    merged

--- hoge.py
+++ hoge.py
@@ -1,5 +1,1 @@
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
